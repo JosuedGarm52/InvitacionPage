78c303b2cb8d3c7a6fddb368bb5d2dde90bc0dfd
Josue Garcia <jaccobjosue@gmail.com>
2025-08-09 16:37:00 -0500

    ajustes al boton auto scroll, func

--- resources/js/controlScroll.js
+++ resources/js/controlScroll.js
@@ -1,6 +1,6 @@
 let autoScrollInterval = null;
 let autoScrollActive = false;
-const SCROLL_DELAY = 5000; // 5 segundos
+const SCROLL_DELAY = 3000; // 3 segundos
 
 function startAutoScroll() {
     if (autoScrollInterval) clearInterval(autoScrollInterval);
@@ -21,22 +21,41 @@ function stopAutoScroll() {
 }
 
 function toggleAutoScroll() {
+    //console.log("toggleAutoScroll called. autoScrollActive =", autoScrollActive);
     if (autoScrollActive) {
+        //console.log("Deteniendo auto scroll");
         stopAutoScroll();
     } else {
         autoScrollActive = true;
         $("#scroll-icon").removeClass("fa-play").addClass("fa-square");
+        //console.log("Iniciando delay de scroll de", SCROLL_DELAY, "ms");
         setTimeout(() => {
-            if (autoScrollActive) startAutoScroll();
+            if (autoScrollActive) {
+                //console.log("Iniciando auto scroll después del delay");
+                startAutoScroll();
+            } else {
+                //console.log("Auto scroll ya no está activo, no inicia");
+            }
         }, SCROLL_DELAY);
     }
 }
 
-// Inicia el scroll automático 5 segundos después de que cargue la página
+
+// Al cargar la página, iniciar scroll después de 5 segundos
 $(window).on("load", function() {
     autoScrollActive = true;
     $("#scroll-icon").removeClass("fa-play").addClass("fa-square");
     setTimeout(() => {
         if (autoScrollActive) startAutoScroll();
     }, SCROLL_DELAY);
+});
+
+$(document).on("click", function(e) {
+    const clickedOnScrollBtn = $(e.target).closest("#scrollToggleBtn").length > 0;
+    const clickedOnAudioControls = $(e.target).closest("#audioControlsContainer").length > 0;
+
+    if (autoScrollActive && !clickedOnScrollBtn && !clickedOnAudioControls) {
+        //console.log("Click fuera de controles, deteniendo autoscroll");
+        stopAutoScroll();
+    }
 });
